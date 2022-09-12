011625282099f40d5a055a6df8956a9270888aae
devin-lynch <devin.l.lynch@gmail.com>
2022-09-12 14:05:20 -0500

    Complete part 1

---
+++ controllers/users.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models')
+
+// GET /users/new -- render a form to create a new user
+router.get('/new', (req, res) => {
+    res.render('users/new.ejs')
+})
+
+// POST /users -- create a new user in the db
+router.post('/', async (req, res) => {
+    try {
+        // create a new user
+        const newUser = await db.user.create(req.body)
+        // store that new user's id as a cookie in the browser
+        // res.cookie('key', value)
+        res.cookie('userId', newUser.id)
+        // redirect to the homepage
+        res.redirect('/')
+
+    } catch(err) {
+        console.warn(err)
+        res.send(`Server Error 💀`)
+    }
+    
+})
+
+module.exports = router
